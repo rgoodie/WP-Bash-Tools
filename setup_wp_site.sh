bbe657a03c75bbedcb3260d261264dd585a535ed
Richard <r.chardgoodrow@gmail.com>
2014-03-26 11:22:03 -0500

    IT WORKED. setup_wp_site creates a site... ready to go!

--- setup_wp_site.sh
+++ setup_wp_site.sh
@@ -8,16 +8,14 @@ DB_PASS=`tr -cd '[:alpha:]' </dev/urandom | base64 | head -c 8 | tr -cd '[:alpha
 SITE_EMAIL=email@example.com
 
 
-
-
 # Get information from user
-echo "Enter information as prompted. Avoid spaces in name fields:"
+echo "Enter information as prompted. Avoid spaces in name fields: \n"
 echo -n "New site/database name: "
 read DB_NAME
 
+
 # Construct other values based on DB_NAME
 DB_USER=$DB_NAME-user
-DB_ADMIN=$DB_NAME-admin
 
 # Create the database, user, and grant privileges
 echo "Enter mysql root admin password:"
@@ -28,10 +26,10 @@ mysql -u $DB_ROOT -p -e "DROP DATABASE IF EXISTS $DB_NAME; CREATE DATABASE $DB_N
 WWW_PATH="/var/www"
 mkdir -p $WWW_PATH
 cd $WWW_PATH
-#wget  -O $WWW_PATH/tmp.tgz http://wordpress.org/latest.tar.gz 
+wget  -O $WWW_PATH/tmp.tgz http://wordpress.org/latest.tar.gz 
 tar xf $WWW_PATH/tmp.tgz
 mv $WWW_PATH/wordpress $WWW_PATH/$DB_NAME
-#rm $WWW_PATH/tmp.tgz
+rm $WWW_PATH/tmp.tgz
 
 # Prep Install 
 wp core config --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=localhost --path=$WWW_PATH/$DB_NAME
@@ -45,9 +43,10 @@ wp core install --url=$SITE_URL --title=\"$SITE_TITLE\" --admin_name=$SITE_ADMIN
 echo =====================
 
 echo SAVE THIS INFO
-echo $SITE_URL/wp-admin
-echo $SITE_ADMIN
-echo $SITE_PASS
-echo $SITE_EMAIL
-echo $DB_PASS
+echo To edit the site visit: $SITE_URL/wp-admin
+echo Admin User  	: $SITE_ADMIN
+echo Admin Pass  	: $SITE_PASS
+echo Admin Email 	: $SITE_EMAIL
+echo Database User 	: $DB_USER
+echo Database Pass	: $DB_PASS
 
