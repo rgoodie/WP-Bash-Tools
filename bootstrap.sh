f63ad206d5b9c30c31a2ca82141775630e8d1915
Richard <r.chardgoodrow@gmail.com>
2014-03-27 09:01:36 -0500

    Uploading changes.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install curl vim wget apache2 php5 libapache2-mod-php5  mysql-server libapache2-mod-auth-mysql php5-mysql git -y
+sudo apt-get install curl vim wget apache2 php5 php5-cli php5-gd php5-mysql libapache2-mod-php5 apache2 libapache2-mod-auth-mysql php5-mysql git -y
 
 
 
