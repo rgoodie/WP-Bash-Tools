c3721acaa8e25f51534cec3a09dd3cad869f124c
Richard <r.chardgoodrow@gmail.com>
2014-03-28 11:53:47 -0500

    Moved wp-cli install temporary folder to $HOME:x

--- install_WPCLI.sh
+++ install_WPCLI.sh
@@ -1,4 +1,6 @@
 # get wp_cli
-curl -L https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /vagrant/wp-cli.phar
-sudo chmod +x /vagrant/wp-cli.phar
-sudo mv wp-cli.phar /usr/bin/wp
+
+
+curl -L https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > $HOME/wp-cli.phar
+sudo chmod a+x $HOME/wp-cli.phar
+sudo mv $HOME/wp-cli.phar /usr/bin/wp
