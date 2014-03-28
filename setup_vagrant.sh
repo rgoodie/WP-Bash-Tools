12b53724a07a141d25715f5f45e439b9792ec30e
Richard <r.chardgoodrow@gmail.com>
2014-03-28 10:02:43 -0500

    Found a nicer Vagrant solution. So I'm changing this project over to use it in the setup_vagrant script.

---
+++ setup_vagrant.sh
@@ -0,0 +1,4 @@
+# http://vagrantpress.org/
+wget -O vagrantpress-master.zip https://github.com/chad-thompson/vagrantpress/archive/master.zip
+unzip vagrantpress-master.zip
+mv vagrantpress-master $1
